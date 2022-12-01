fabd0d6bfcebb03a5e8515945300ee50d5d72480
Maya Copeland <maya.copeland@protonmail.com>
2022-12-01 21:41:33 +0000

    Use more 'functional' stuff

--- src/days.rs
+++ src/days.rs
@@ -8,24 +8,15 @@ pub fn day_all(token: String) {
 pub fn day_one(token: String) {
     println!("Day One: ");
     downloader::get_task(1, token);
-    let file = read_to_string("src/task_input/task1.txt").unwrap();
-    let file_split: Vec<&str> = file.split("\n\n").collect();
-    let mut sum_vector: Vec<i32> = vec!(); 
     
-    // TODO: improve hacky code
-    for str in file_split {
-        let elf: Vec<&str> = str.split("\n").collect();
-        let mut sum = 0;
-        for calorie in elf {
-            if calorie != "" {
-                let calorie_int = calorie.parse::<i32>().unwrap();
-                sum += calorie_int;
-            }
-        }
-        sum_vector.push(sum);
-    }
-    let mut cloned = sum_vector.clone();
-    let max = sum_vector.iter().max().unwrap();
+    let file: String = read_to_string("src/task_input/task1.txt").unwrap();
+    let elves: Vec<i32> = file.split("\n\n").map(|xs| {
+        let elf: Vec<i32> = xs.split("\n").map(|x| x.parse::<i32>().unwrap_or(0)).collect(); 
+        let sum: i32 = elf.iter().sum();
+        sum
+    }).collect();
+    let mut cloned: Vec<i32> = elves.clone();
+    let max: &i32 = elves.iter().max().unwrap();
 
     println!("Part One Solution: {}", max);
     
